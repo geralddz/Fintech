89ffe367dd23b7af0cc6d9240fd56b461710b8f7
GeraldDZ <dzulfiqarg@gmail.com>
2022-03-28 05:31:04 +0700

    activity tambahan

---
+++ app/src/main/java/com/mobile/fintech/ActivityTambahan.kt
@@ -0,0 +1,11 @@
+package com.mobile.fintech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTambahan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambahan)
+    }
+}
